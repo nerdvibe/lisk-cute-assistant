a75c635ca538ba3a5614ce2e93987ba38080947d
xunga <6796782+xunga@users.noreply.github.com>
2018-08-22 20:53:13 +0100

    prettified codebase

--- src/lib/welcome.js
+++ src/lib/welcome.js
@@ -1,12 +1,11 @@
-import { sendSMSWelcome } from './sms'
+import { sendSMSWelcome } from "./sms";
 import settings from "../config";
-import {respondServerStatus} from "./server";
-import {bot} from "./telegram";
+import { respondServerStatus } from "./server";
+import { bot } from "./telegram";
 
-export const welcome = async() => {
-
+export const welcome = async () => {
   // Welcome sms message
-  sendSMSWelcome('Lisk Cute Assistant on ' + settings.nodeName + ' rebooted');
+  sendSMSWelcome("Lisk Cute Assistant on " + settings.nodeName + " rebooted");
   // Welcome chat message
   if (settings.chatId && settings.rebootWelcome) {
     await bot.reply(
@@ -14,5 +13,4 @@ export const welcome = async() => {
     );
     respondServerStatus();
   }
-
 };
