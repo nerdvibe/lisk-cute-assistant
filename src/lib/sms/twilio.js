22b20ba6cb9d0992ddc6f383dd281a9ff87f08c8
TonyMorinello <MorinelloA@gmail.com>
2018-08-22 21:02:18 -0400

    Conflict correction
    Corrected a conflict with previous Twilio refactor commit

--- src/lib/sms/twilio.js
+++ src/lib/sms/twilio.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const querystring = require("querystring");
 const axios = require("axios");
 const settings = require("../../config");
@@ -27,43 +26,5 @@ const sendTwilio = async (message) =>
 
   return response;
 }
-=======
-import request from "request";
-import settings from "../../config";
-
-const sendTwilio = async message => {
-  const dataString =
-    "From=" +
-    settings.twilioData.fromPhoneNumber +
-    "&To=" +
-    settings.twilioData.toPhoneNumber +
-    "&Body=" +
-    message;
-
-  const options = {
-    url:
-      "https://api.twilio.com/2010-04-01/Accounts/" +
-      settings.twilioData.username +
-      "/Messages",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
-    },
-    body: dataString,
-    auth: {
-      user: settings.twilioData.username,
-      pass: settings.twilioData.password
-    }
-  };
-
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
-    }
-  }
-
-  request(options, callback);
-};
->>>>>>> b3e9b0a8588324052e7d337482c3a51cc8b3a34c
 
 exports.sendTwilio = sendTwilio;
